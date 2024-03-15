47c902d4783883ef0267050769d23cfae1cde5e2
DaniLucky06 <musner.daniel@gmail.com>
2024-03-15 10:58:23 +0100

    scuola, added background blur and rem units

--- main.js
+++ main.js
@@ -2,6 +2,8 @@
 const parallaxElement = document.querySelector('.parallax');
 // Get the second element with "parallax" class
 const secondParallaxElement = document.querySelectorAll('.parallax')[1];
+//body element
+const bodyElement = document.querySelector('body');
 
 
 //get the <div class="main"> element
@@ -19,6 +21,11 @@ function animateParallax() {
     const newYPosition = scrollPosition * 0.5; // Adjust the multiplier as needed
     titleElement.style.transform = `translate(-50%, ${newYPosition - titleElement.clientHeight}px)`;
     titleElement.style.opacity = Math.max(1 - scrollPosition / 1000, 100 / scrollPosition);
+
+    parallaxElement.style.backdropFilter = `blur(${scrollPosition / 50}px)`;
+    mainElement.style.backdropFilter = `blur(${scrollPosition / 50}px)`;
+    // opacity relative to scroll position and window height
+    mainElement.style.opacity = scrollPosition / window.innerHeight;
 
     // Call the animation function on the next frame
     requestAnimationFrame(animateParallax);
